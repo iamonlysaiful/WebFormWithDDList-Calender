937d9bcb58d037db94bbb4a100cf4b7012947ba5
iamonlysaiful <tuhinbaj@gmail.com>
2017-02-23 23:50:11 +0600

    Final Committed

---
+++ Default.aspx
@@ -0,0 +1,106 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 60%;
+        }
+        .auto-style2 {
+        }
+        .auto-style3 {
+            height: 26px;
+        }
+        .auto-style4 {
+            width: 206px;
+        }
+        .auto-style5 {
+            height: 26px;
+            width: 206px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1 style="text-align:center;">Doctor Information Form</h1>
+        <fieldset class="auto-style1">
+            <legend>Doctor Infromatin</legend>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style4">Doctor ID :</td>
+                <td>
+                    <asp:TextBox ID="txtDId" runat="server"></asp:TextBox> *Search By 'Doctor ID' 
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Doctor Name :</td>
+                <td>
+                    <asp:TextBox ID="txtDName" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style5">Email :</td>
+                <td class="auto-style3">
+                    <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Appointed Date :</td>
+                <td>
+                    <asp:Literal ID="Literal2" runat="server"></asp:Literal>
+                    <asp:Calendar ID="calSDay" runat="server"   OnLoad="calSDay_Load" OnSelectionChanged="calSDay_SelectionChanged" Height="71px" Width="160px"  ></asp:Calendar>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Department :</td>
+                <td>
+                    <asp:DropDownList ID="dlistDepartment" runat="server">
+                        <asp:ListItem Selected="True">-- Please Select --</asp:ListItem>
+                      
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Room No :</td>
+                <td>
+                    <asp:DropDownList ID="dListRoomNo" runat="server">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">&nbsp;</td>
+                <td>&nbsp;</td>
+            </tr>
+            <tr>
+                <td class="auto-style2" colspan="2">
+                    <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+                    <asp:Button ID="btnSearch" runat="server" OnClick="btnSearch_Click" Text="Search" />
+                    <asp:Button ID="btnEdit" runat="server" Text="Edit" style="width: 37px" OnClick="btnEdit_Click" />
+                    <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" OnClientClick="return confirm('The selected user information will be deleted. Do you wish to continue?'); return false;"/>
+                    <asp:Button ID="btnCancel" runat="server" OnClick="btnCancel_Click" Text="Cancel" />
+                </td>
+            </tr>
+        </table>
+              </fieldset>
+        <br />
+
+        <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+            <Columns>
+                <asp:BoundField DataField="DoctorId" HeaderText="Doctor Id" />
+                <asp:BoundField DataField="dName" HeaderText="Name" />
+                <asp:BoundField DataField="dEmail" HeaderText="Email" />
+                <asp:BoundField DataField="dScheduleDay" HeaderText="Appointed Date" />
+                <asp:BoundField DataField="DepartmentName" HeaderText="Department Name" />
+                <asp:BoundField DataField="RoomNo" HeaderText="Room No" />
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    dbEntities1 db = new dbEntities1();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        btnDelete.Visible = false;
+        btnEdit.Visible = false;
+        btnCancel.Visible = false;
+        refreshAll();
+    }
+
+    private void refreshAll()
+    {
+
+        var data = db.Doctors.Select(d => new { d.DoctorID, d.dName, d.dEmail, d.dScheduleDay, d.Department.DepartmentName, d.Room.RoomNo }).ToList();
+        GridView1.DataSource = data;
+        GridView1.DataBind();
+
+        if (!IsPostBack)
+        {
+
+         
+            
+          
+            populateDropdown();
+        }
+    }
+
+    private void populateDropdown()
+    {
+        //Popolating Data From Foreign Key Table in DropDownlist
+
+        var src = (from d in db.Departments
+                   select new
+                   {
+                       d.DepartmentId,
+                       d.DepartmentName
+                   }).ToList();
+        
+        dlistDepartment.DataSource = src;
+    
+        dlistDepartment.DataTextField = "DepartmentName";
+        dlistDepartment.DataValueField = "DepartmentId";
+        dlistDepartment.DataBind();
+        dlistDepartment.Items.Insert(0, "--Please Select--");
+
+        var src1 = (from r in db.Rooms
+                    select r).ToList();
+  
+        dListRoomNo.DataSource = src1;
+     
+        dListRoomNo.DataTextField = "RoomNo";
+        dListRoomNo.DataValueField = "RoomId";
+        dListRoomNo.DataBind();
+        dListRoomNo.Items.Insert(0, "--Please Select--");
+
+        //Popolating Data From Foreign Key Table in DropDownlist
+    }
+
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+
+        try
+        {
+            Doctor d = new Doctor();
+            d.DoctorID = Int32.Parse(txtDId.Text);
+            d.dName = txtDName.Text;
+            d.dEmail = txtEmail.Text;
+            d.dScheduleDay = calSDay.SelectedDate.ToShortDateString();
+            d.DepartmentId = Int32.Parse(dlistDepartment.SelectedValue);
+            d.RoomId = Int32.Parse(dListRoomNo.SelectedValue);
+
+            //Existing Record Checking Validation
+
+            bool recordtExists = db.Doctors.Any(dn => dn.DoctorID.Equals(d.DoctorID));
+            //Existing Record Checking Validation
+            if (recordtExists)
+            {
+                refreshAll();
+                Literal1.Text = "Data Existed Before!!!";
+
+                btnDelete.Visible = false;
+                btnEdit.Visible = false;
+                btnCancel.Visible = false;
+                btnSave.Visible = true;
+                btnSearch.Visible = true;
+                Clear();
+            }
+            else
+            {
+                db.Doctors.Add(d);
+                db.SaveChanges();
+                Clear();
+                refreshAll();
+                Literal1.Text = "Data Inserted Successfully!!!";
+
+                btnDelete.Visible = false;
+                btnEdit.Visible = false;
+                btnCancel.Visible = false;
+                btnSave.Visible = true;
+                btnSearch.Visible = true;
+            }
+        }
+        catch (Exception ex)
+        {
+
+            Literal1.Text = ex.Message;
+        }
+
+
+    }
+
+    private void Clear()
+    {
+        txtDId.Text = "";
+        txtDName.Text = "";
+        txtEmail.Text = "";
+        Literal2.Text = "";
+        populateDropdown();
+    }
+
+
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            int idn = Int32.Parse(txtDId.Text);
+            var data = (from d in db.Doctors
+                        where d.DoctorID == idn
+                        select d).FirstOrDefault();
+
+
+            if (data != null)
+            {
+                txtDId.Text = data.DoctorID.ToString();
+                txtDName.Text = data.dName;
+                txtEmail.Text = data.dEmail;
+                Literal2.Text = data.dScheduleDay;
+                calSDay.SelectedDate = DateTime.Parse(data.dScheduleDay);
+                dlistDepartment.SelectedValue = data.DepartmentId.ToString();
+                dListRoomNo.SelectedValue = data.RoomId.ToString();
+                Literal1.Text = "";
+                btnDelete.Visible = true;
+                btnEdit.Visible = true;
+                btnSearch.Visible = true;
+                btnSave.Visible = false;
+                btnCancel.Visible = true;
+            }
+            else
+            {
+                Literal1.Text = "Data Not Found!!!";
+                Clear();
+                btnSearch.Visible = true;
+                btnSave.Visible = true;
+            }
+        }
+        catch (Exception ex1)
+        {
+            Literal1.Text = ex1.Message;
+        }
+    }
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+
+        int idn = Int32.Parse(txtDId.Text);
+
+        var data = (from d in db.Doctors
+                    where d.DoctorID == idn
+                    select d).FirstOrDefault();
+
+        if (data != null)
+        {
+            db.Doctors.Remove(data);
+            db.SaveChanges();
+
+            Clear();
+            refreshAll();
+            Literal1.Text = "Data Deleted Successfully!";
+            btnSearch.Visible = true;
+            btnSave.Visible = true;
+        }
+        else
+        {
+            Literal1.Text = "Data Not Found!!!";
+            Clear();
+            btnSearch.Visible = true;
+            btnSave.Visible = true;
+        }
+
+    }
+
+    protected void calSDay_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+
+        }
+
+
+
+    }
+    protected void btnEdit_Click(object sender, EventArgs e)
+    {
+        int idn = Int32.Parse(txtDId.Text);
+        var data = (from d in db.Doctors
+                    where d.DoctorID == idn
+                    select d).FirstOrDefault();
+
+        if (data != null)
+        {
+            data.DoctorID = Int32.Parse(txtDId.Text);
+            data.dName = txtDName.Text;
+            data.dEmail = txtEmail.Text;
+            data.dScheduleDay = Literal2.Text;
+            data.DepartmentId = Int32.Parse(dlistDepartment.SelectedValue);
+            data.RoomId = Int32.Parse(dListRoomNo.SelectedValue);
+            db.SaveChanges();
+            Literal1.Text = "Data Edited Successfully!";
+            refreshAll();
+            Clear();
+
+            btnDelete.Visible = false;
+            btnEdit.Visible = false;
+            btnSearch.Visible = true;
+            btnSave.Visible = true;
+            btnCancel.Visible = false;
+
+        }
+
+    }
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+        btnDelete.Visible = false;
+        btnEdit.Visible = false;
+        btnSearch.Visible = true;
+        btnSave.Visible = true;
+        btnCancel.Visible = false;
+
+        Clear();
+    }
+    protected void calSDay_SelectionChanged(object sender, EventArgs e)
+    {
+        Literal2.Text = calSDay.SelectedDate.ToShortDateString();
+        //btnEdit.Visible = true;
+       btnCancel.Visible = true;
+    }
+
+   
+}
